f85ecffc2ccf9207b464a0cadc67ed0c865b1d54
Feng <1218498963@.com>
2022-10-11 17:24:30 +0800

    fix--ERROR CHECK

--- server/transfer_file.c
+++ server/transfer_file.c
@@ -19,8 +19,9 @@ int send_circle(int fd, const char * buf, int length)
 	return length - left;
 }
 
-int transfer_file(int peerfd, const char * filename)
+void transfer_file(int peerfd, const char * filename)
 {
+	int res;
   int fds[2];
 	pipe(fds);
 
@@ -28,21 +29,23 @@ int transfer_file(int peerfd, const char * filename)
 	int rfd = open(filename, O_RDWR);
 	ERROR_CHECK(rfd, -1, "open");
   
-	//获取文件名长度
 	Mudhead_t truck;
 	memset(&truck, 0, sizeof(truck));
+
 	truck.length = strlen(filename);
 	strncpy(truck.data, filename, truck.length);
   
 	//传送文件名
-	send_circle(peerfd, (const char *)&truck, truck.length);
+	res = send_circle(peerfd, (const char *)&truck, truck.length);
+  ERROR_CHECK(res, -1, "send filename");
 
 	//传送文件内容
   struct stat st;
 	fstat(rfd, &st);
 	printf("file length:%ld\n", st.st_size);
-  send_circle(rfd, (const char*)&st.st_size, st.st_size);
-	
+  res = send_circle(rfd, (const char*)&st.st_size, st.st_size);
+	ERROR_CHECK(res, -1, "send file_content");
+
 	char* pbuf = (char *)mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, rfd, 0);
 	int ret = send(peerfd, pbuf, st.st_size, 0);
 	printf("send ret:%d\n", ret);
