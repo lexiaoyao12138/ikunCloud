ae537e36ac67de45c35c9e0e9edbe659bf8d7885
zhao <1845763150@qq.com>
2022-10-11 20:43:17 +0800

    rm command_cd.c add to system.c

--- server/system.c
+++ server/system.c
@@ -1,7 +1,16 @@
 #include "system.h"
 
-int command_cd(char *){
+struct user_info{
+    int peerfd;//用户客户端套接字
+    char path[1024];//用户当前工作目录
+
+};
+
+int command_cd(char* arg,struct user_info){
+    int ret = chdir(arg);
+    ERROR_CHECK(ret,-1,"chdir");
     
+    return 0;
 }
 
 int command_ls(char *)
