b5fc719b27464f13512eff7a6110e099bbfc9cc1
Feng <1218498963@.com>
2022-10-11 21:15:30 +0800

    fix bugs from client.c

--- client/client.c
+++ client/client.c
@@ -1,6 +1,6 @@
 #include "../public/public.h"
 
-int client_init(char *host, int port) {
+int main( ) {
   // 1. 创建监听套接字
   int clientfd = socket(AF_INET, SOCK_STREAM, 0);
   ERROR_CHECK(clientfd, -1, "socket");
@@ -8,8 +8,8 @@ int client_init(char *host, int port) {
   struct sockaddr_in serveraddr;
   bzero(&serveraddr, sizeof(serveraddr));
   serveraddr.sin_family = AF_INET;
-  serveraddr.sin_port = htons(port);
-  serveraddr.sin_addr.s_addr = inet_addr(host);
+  serveraddr.sin_port = htons(8888);
+  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 
  //2.1 直接连接服务器
