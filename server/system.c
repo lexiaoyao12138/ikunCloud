92fe12b09b608646318ba53e9ff74ca310f09ee8
zhao <1845763150@qq.com>
2022-10-12 14:12:59 +0800

    add commed_ls

--- server/system.c
+++ server/system.c
@@ -1,9 +1,29 @@
 #include "system.h"
 
+
 int command_cd(char* arg,struct user_info* u_info){
     int ret = chdir(arg);
     ERROR_CHECK(ret,-1,"chdir");
     strcpy(u_info->path,arg);
+    return 0;
+}
+
+int command_ls(char *path){
+    //打开目录流
+    DIR* pdir = opendir(path);
+    ERROR_CHECK(pdir,NULL,"opendir");
+    //读目录流
+    struct dirent* pdirent;
+    errno = 0;
+    int length = 0;
+    while((pdirent = readdir(pdir)) != NULL){
+        char *name = pdirent -> d_name;
+        if(name[0] == '.'){
+            continue;
+        }
+        sprintf(path+length,"%s|",name);
+        length += strlen(name)+1;
+    }
     return 0;
 }
 
@@ -29,4 +49,9 @@ int command_mkdir(char *path)
     int ret = mkdir(path,0666);
     ERROR_CHECK(ret,-1,"mkdir");
     return 0 ;
+}
+
+int main(void){
+    char path[] = "./";
+    command_ls(path);
 }
