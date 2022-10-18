61aa36cf44112378aedc43d5b9a7fd736ad5fc5d
lexiaoyao12138 <1639503985@qq.com>
2022-10-18 17:00:51 +0800

    refactoring

--- Public/public.h
+++ Public/public.h
@@ -1,5 +1,5 @@
-#ifndef _PUBLIC_H_
-#define _PUBLIC_H_
+#ifndef _PUBLIC_H
+#define _PUBLIC_H
 
 #include "cjson/cJSON.h"
 #include <arpa/inet.h>
@@ -7,11 +7,13 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <grp.h>
+#include <mysql/mysql.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <pwd.h>
 #include <setjmp.h>
+#include <shadow.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,21 +32,33 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/wait.h>
+#include <syslog.h>
 #include <time.h>
 #include <unistd.h>
-#include <shadow.h>
-#include <syslog.h>
-#include <mysql/mysql.h>
-
 
 #define CURRENT_PATH "../userDir"
 #define CONFIG_PATH "../config/server.json"
-#define IPLEN	20   /*ip的长度*/
+#define IPLEN 20 /*ip的长度*/
+
+typedef enum {
+  COMMAND_CD = 2,
+  COMMAND_LS = 3,
+  COMMAND_PWD = 4,
+  COMMAND_PUT = 5,
+  COMMAND_GET = 6,
+  COMMAND_RM = 7,
+  COMMAND_MKDIR = 8,
+} command_type;
+
+// TODO: 
+// 发送文件
+// 接收文件
+
 
 #define ERROR_CHECK(ret, res, name)                                            \
   {                                                                            \
     if (ret == res) {                                                          \
-      printf("%s:on line %d\n", __FILE__, __LINE__);                            \
+      printf("%s:on line %d\n", __FILE__, __LINE__);                           \
       perror(name);                                                            \
       exit(0);                                                                 \
     }                                                                          \
@@ -57,20 +71,4 @@
     }                                                                          \
   } while (0);
 
-typedef enum {
-  COMMAND_CD = 2,
-  COMMAND_LS = 3,
-  COMMAND_PWD = 4,
-  COMMAND_PUT = 5,
-  COMMAND_GET = 6,
-  COMMAND_RM = 7,
-  COMMAND_MKDIR = 8,
-} command_type;
-
-
-int send_circle(int, const char*, int);
-int recv_circle(int, char*, int);
-void send_file(int, const char*);
-void recv_file(int);
-
-#endif
+#endif // _PUBLIC_H
